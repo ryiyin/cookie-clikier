75c05212643b724c74314bcceb39da883451e232
ryiyin <90423334+ryiyin@users.noreply.github.com>
2023-05-01 23:42:13 -0400

    LAYOUT

--- src/Cookie.jsx
+++ src/Cookie.jsx
@@ -1,11 +1,26 @@
-
+import { useState } from "react";
 
 export function Cookie() {
+  const [cookieClick, setCookieclick] = useState(0);
   return (
-    <div>
-      <h1>asd</h1>
-      <button>a</button>
-      <img src={"../src/cookie_PNG13659.png"} alt="ds" className="cookie" onClick={"this.classList.toggle('agrandar')"}/>
+    <div className="container">
+      <div className="column column-cookie">
+        <h1 className="cookie-counter">{cookieClick}</h1>
+        <img
+          src={"../src/assets/cookie_PNG13659.png"}
+          alt="ds"
+          className="cookie"
+          onMouseUp={() => {
+            setCookieclick(cookieClick + 1);
+          }}
+        />
+      </div>
+      <div>
+        imagen
+      </div>
+      <div className="column updgrades">
+        upgrades
+      </div>
     </div>
   );
 }
